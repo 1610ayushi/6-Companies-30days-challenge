86493760d5380aff340e33b4a7ed867f03559aff
Ayushi Tiwari <tayushi1610@gmail.com>
2023-01-09 19:40:40 +0530

    Create Que09.java

---
+++ Goldman Sachs/Que09.java
@@ -0,0 +1,64 @@
+/*
+Suppose LeetCode will start its IPO soon. In order to sell a good price of its shares to Venture Capital, LeetCode would like to work on some projects to increase its capital before the IPO. Since it has limited resources, it can only finish at most k distinct projects before the IPO. Help LeetCode design the best way to maximize its total capital after finishing at most k distinct projects.
+
+You are given n projects where the ith project has a pure profit profits[i] and a minimum capital of capital[i] is needed to start it.
+
+Initially, you have w capital. When you finish a project, you will obtain its pure profit and the profit will be added to your total capital.
+
+Pick a list of at most k distinct projects from given projects to maximize your final capital, and return the final maximized capital.
+
+The answer is guaranteed to fit in a 32-bit signed integer.
+
+ 
+
+Example 1:
+
+Input: k = 2, w = 0, profits = [1,2,3], capital = [0,1,1]
+Output: 4
+Explanation: Since your initial capital is 0, you can only start the project indexed 0.
+After finishing it you will obtain profit 1 and your capital becomes 1.
+With capital 1, you can either start the project indexed 1 or the project indexed 2.
+Since you can choose at most 2 projects, you need to finish the project indexed 2 to get the maximum capital.
+Therefore, output the final maximized capital, which is 0 + 1 + 3 = 4.
+Example 2:
+
+Input: k = 3, w = 0, profits = [1,2,3], capital = [0,1,2]
+Output: 6
+ 
+
+Constraints:
+
+1 <= k <= 105
+0 <= w <= 109
+n == profits.length
+n == capital.length
+1 <= n <= 105
+0 <= profits[i] <= 104
+0 <= capital[i] <= 109
+*/
+public int findMaximizedCapital(int numberOfProjects, int initialCapital, int[] profits, int[] capital) {
+    int n = profits.length;
+    PriorityQueue<Integer> minCapitalHeap = new PriorityQueue<>(n, (i1, i2) -> capital[i1] - capital[i2]);
+    PriorityQueue<Integer> maxProfitHeap = new PriorityQueue<>(n, (i1, i2) -> profits[i2] - profits[i1]);
+
+    // insert all project capitals to a min-heap
+    for (int i = 0; i < n; i++)
+      minCapitalHeap.offer(i);
+
+    // let's try to find a total of 'numberOfProjects' best projects
+    int availableCapital = initialCapital;
+    for (int i = 0; i < numberOfProjects; i++) {
+      // find all projects that can be selected within the available capital and insert them in a max-heap
+      while (!minCapitalHeap.isEmpty() && capital[minCapitalHeap.peek()] <= availableCapital)
+        maxProfitHeap.add(minCapitalHeap.poll());
+
+      // terminate if we are not able to find any project that can be completed within the available capital
+      if (maxProfitHeap.isEmpty())
+        break;
+
+      // select the project with the maximum profit
+      availableCapital += profits[maxProfitHeap.poll()];
+    }
+
+    return availableCapital;
+  }
