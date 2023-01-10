64a97d251f1d7969734d496966666d40d12f2c61
Ayushi Tiwari <tayushi1610@gmail.com>
2023-01-10 19:51:30 +0530

    Create Que14.java

---
+++ Goldman Sachs/Que14.java
@@ -0,0 +1,46 @@
+/*
+You are given an array nums that consists of non-negative integers. Let us define rev(x) as the reverse of the non-negative integer x. For example, rev(123) = 321, and rev(120) = 21. A pair of indices (i, j) is nice if it satisfies all of the following conditions:
+
+0 <= i < j < nums.length
+nums[i] + rev(nums[j]) == nums[j] + rev(nums[i])
+Return the number of nice pairs of indices. Since that number can be too large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: nums = [42,11,1,97]
+Output: 2
+Explanation: The two pairs are:
+ - (0,3) : 42 + rev(97) = 42 + 79 = 121, 97 + rev(42) = 97 + 24 = 121.
+ - (1,2) : 11 + rev(1) = 11 + 1 = 12, 1 + rev(11) = 1 + 11 = 12.
+Example 2:
+
+Input: nums = [13,10,35,24,76]
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 109
+*/
+    public int countNicePairs(int[] A) {
+        int res = 0, mod = (int)1e9 + 7;
+        Map<Integer, Integer> count = new HashMap<>();;
+        for (int a : A) {
+            int b = rev(a), v = count.getOrDefault(a - b, 0);
+            count.put(a - b, v + 1);
+            res = (res + v) % mod;
+        }
+        return res;
+    }
+
+    public int rev(int a) {
+        int b = 0;
+        while (a > 0) {
+            b = b * 10 + (a % 10);
+            a /= 10;
+        }
+        return b;
+    }
